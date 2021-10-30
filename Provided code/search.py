16347d88c9add746172ebf74964c777ad0719d6f
Behdad Mansouri <behdadaspacey2099@gmail.com>
2021-10-30 23:51:43 +0330

    UCS working!

--- Provided code/search.py
+++ Provided code/search.py
@@ -125,9 +125,10 @@ def uniformCostSearch(problem):
         node, path = fringe.pop()
         closed.push(node)
         if problem.isGoalState(node):
-            return path.list
+            return path
         for child in problem.getSuccessors(node):
-            fringe.update((child[0], path + [child[1]]), problem.getCostOfActions(path + [child[1]]))
+            if child[0] not in closed.list:
+                fringe.update((child[0], path + [child[1]]), problem.getCostOfActions(path + [child[1]]))
 
 def nullHeuristic(state, problem=None):
     """
